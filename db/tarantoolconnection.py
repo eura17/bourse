f04f7dfc252016d5d36b7d5bd815706110233689
eura17 <andrey.babkin.ru71@gmail.com>
2021-01-21 21:17:07 +0300

    create ctxt manager

--- db/tarantoolconnection.py
+++ db/tarantoolconnection.py
@@ -1,63 +1,53 @@
-from typing import NoReturn
-from abc import ABC
 import os
+import shutil
 import subprocess
 from time import sleep
 
+from db.user import User
 
-class TarantoolConnection(ABC):
-    _HOST = None
-    _PORT = None
-    __tarantool_subprocess = None
 
-    @classmethod
-    def set_host(cls, value: str) -> NoReturn:
-        cls._HOST = value
+class TarantoolConnection:
+    __work_dir = 'tarantool_work_dir'
 
-    @classmethod
-    def set_port(cls, value: int) -> NoReturn:
-        cls._PORT = value
-
-    @classmethod
-    def launch(cls,
-               port: int) -> NoReturn:
-        cls.__tarantool_subprocess = subprocess.Popen(
+    def __init__(self, port: int):
+        self.__tarantool_subprocess = subprocess.Popen(
             ['tarantool'],
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE
         )
-        cls.__configure(port)
-        cls.__migrate()
-        cls.__tarantool_subprocess.stdin.close()
+        self.__port = port
+
+    def __enter__(self):
+        self.__configure(self.__port)
+        self.__migrate()
+        self.__tarantool_subprocess.stdin.close()
         sleep(1)
-        cls.set_host('localhost')
-        cls.set_port(port)
+        User.set_host('localhost')
+        User.set_port(self.__port)
 
-    @classmethod
-    def __configure(cls, port: int):
-        path = cls.__get_dir()
-        if 'work_dir' not in os.listdir(path):
-            os.mkdir(f'{path}/work_dir')
-        path = f'{path}/work_dir'
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.__tarantool_subprocess.kill()
+
+    def __configure(self, port: int):
+        base_path = self.__get_dir()
+        if self.__work_dir in os.listdir(base_path):
+            shutil.rmtree(self.__work_dir)
+        os.mkdir(f'{base_path}/{self.__work_dir}')
+        path = f'{base_path}/{self.__work_dir}'
         cfg = f'box.cfg{{listen={port}, work_dir="{path}"}}'
-        cls.__tarantool_subprocess.stdin.write(bytes(cfg, encoding='utf-8'))
+        self.__tarantool_subprocess.stdin.write(bytes(cfg, encoding='utf-8'))
 
-    @classmethod
-    def __migrate(cls):
-        if cls.__tarantool_subprocess is not None:
-            path = cls.__get_dir()
+    def __migrate(self):
+        if self.__tarantool_subprocess is not None:
+            path = self.__get_dir()
             migrations = ''
             for file in os.listdir(f'{path}/migrations'):
                 with open(f'{path}/migrations/{file}') as f:
                     migrations += f.read().strip()
                     migrations += '\n\n\n'
-            cls.__tarantool_subprocess.stdin.write(bytes(migrations,
-                                                         encoding='utf-8'))
+            self.__tarantool_subprocess.stdin.write(bytes(migrations,
+                                                          encoding='utf-8'))
 
     @staticmethod
     def __get_dir():
         return os.path.dirname(os.path.realpath(__file__))
-
-    @classmethod
-    def kill(cls):
-        cls.__tarantool_subprocess.kill()
--- db/user.py
+++ db/user.py
@@ -1,10 +1,21 @@
+from typing import NoReturn
+from abc import ABC
 import tarantool
 
-from db.tarantoolconnection import TarantoolConnection
 
+class User(ABC):
+    _HOST = None
+    _PORT = None
 
-class User(TarantoolConnection):
     def __init__(self,
                  user: str = None,
                  password: str = None):
         self._conn = tarantool.connect(self._HOST, self._PORT, user, password)
+
+    @classmethod
+    def set_host(cls, value: str) -> NoReturn:
+        cls._HOST = value
+
+    @classmethod
+    def set_port(cls, value: int) -> NoReturn:
+        cls._PORT = value
